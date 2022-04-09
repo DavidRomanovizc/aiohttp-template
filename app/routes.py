e0cc96cd3c3918c36ace87d175c9bce378aae85d
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-04-09 21:00:01 +0300

    🎉 Create routes

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from app.views import frontend
+
+
+def setup_routes(app):
+    app.router.add_static('/js/', path='templates/client/js', name='index.js', show_index=True)
+    app.router.add_static('/css/', path='templates/client/css', name='style.css', show_index=True)
+    app.router.add_static('/font/', path='templates/client/font', name='font', show_index=True)
+    app.router.add_route('GET', '/', frontend.index)
