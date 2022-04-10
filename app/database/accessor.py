62a8478b8400ee0a9c5cde7b8dbebae6da44e2e6
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-04-10 15:45:28 +0300

    ✨ Code for connection to database

---
+++ app/database/accessor.py
@@ -0,0 +1,15 @@
+import asyncpgsa
+
+
+def setup(app):
+    app.on_startup.append(_on_connect)
+    app.on_cleanup.append(_on_shutdown)
+
+
+async def _on_connect(app):
+    config = app["config"]
+    app['db'] = await asyncpgsa.create_pool(dsn=config['database_url'])
+
+
+async def _on_shutdown(app):
+    await app['db'].close()
