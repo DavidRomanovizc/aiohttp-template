248dde3e18926bb795591f3e6b48231c81658d07
DavidRomanovizc <dtdzhalaev@gmail.com>
2022-04-12 12:29:09 +0300

    ♻️ Update

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,17 @@
+import asyncio
+import platform
 from pathlib import Path
+
+import aioreloader
 import yaml
+from loguru import logger
+
+from app.utils.args_parser import setup_args
 
 BASE_DIR = Path(__file__).parent.parent
 __all__ = ('load_config',)
 config_path = BASE_DIR / 'config' / 'config.yaml'
+args = setup_args()
 
 
 def load_config(config_file=None):
@@ -15,3 +23,18 @@ def load_config(config_file=None):
 
     config.update(**cf_dict)
     return config
+
+
+def project_settings():
+    try:
+        import uvloop
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    except Exception as err:
+        logger.info(f"Uvloop is not available - {err}")
+
+    if platform.system() == "Windows":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    if setup_args().reload:
+        logger.info("Start with code reload")
+        aioreloader.start()
